17ec5c1aaa66ca2f11a13a6fbb3a7c5eb870beec
Bernard Baltrusaitis <bernard.baltrusaitis@contino.io>
2019-03-27 11:09:25 +1100

    adding more memory for the docker service

--- main.go
+++ main.go
@@ -13,5 +13,3 @@ func main() {
 	restful.Add(home.New())
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
-// go run -ldflags "-X github.com/b-b3rn4rd/omni-task-two/resouces/home.Version=1.0.1" main.go
